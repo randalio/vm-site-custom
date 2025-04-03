3ff43701e2fba1882b7b3fc88cc97955867dc140
Randal Pope <randalpope@Randals-MacBook-Air.local>
2025-04-03 09:20:39 -0400

    updated plugin

--- vm-site-custom.php
+++ vm-site-custom.php
@@ -3,7 +3,7 @@
  * Plugin Name: Vinyl Marketing Site Customizations
  * Plugin URI: 
  * Description: 
- * Version: 0.0.7
+ * Version: 0.0.8
  * Author: Randal Pope
  * Author URI: https://randal.io
  * License: GPL-2.0+
@@ -13,7 +13,7 @@
 
 
 function plugin_version(){
-	$version = '0.0.7';
+	$version = '0.0.8';
 	return $version;
 }
 
