02670175e4bbc34bd791ec8c7f0b2d23bd9ab841
Randal Pope <randalpope@Randals-MacBook-Air.local>
2025-04-04 14:11:16 -0400

    updated some styles for LP launch

--- src/js/main.js
+++ src/js/main.js
@@ -11,34 +11,91 @@ class VinylPluginJS {
             // Initialize Locomotive Scroll
 
 
-            // const sections = document.querySelectorAll('.entry-content .wp-block-kadence-rowlayout');
+            //const modalElement = document.getElementById('#videomodal'); // Replace with your modal's actual ID
 
-            // sections.forEach((section, index) => {
-            //     section.setAttribute('data-scroll-section', '');
-            // });
+            function addClassNameListener(elemId, callback) {
+                var elem = document.getElementById(elemId);
+                var lastClassName = elem.className;
+                window.setInterval( function() {   
+                   var className = elem.className;
+                    if (className !== lastClassName) {
+                        callback();   
+                        lastClassName = className;
+                    }
+                },10);
+            }
 
-            // document.getElementById('colophon').setAttribute('data-scroll-section', '');
 
-            // const scroll = new LocomotiveScroll({
-            //   el: document.querySelector('[data-scroll-container]'),
-            //   smooth: true,
-            //   lerp: 0.1, // Start with a moderately responsive lerp
-            //   multiplier: 0.8, // Multiplier for scroll speed
-            //   scrollFromAnywhere: true
-            // });
+            if( document.querySelectorAll('#videomodal').length > 0 ) {
+                            // get #videomodal's inner HTML
+                const modalContent = document.getElementById('videomodal').innerHTML;
+
+                addClassNameListener('videomodal', function() {
+                    const modalElement = document.getElementById('videomodal');
+                    const modalVideo = modalElement.querySelector('video');
+
+                    if (modalElement.classList.contains('is-open')) {
+                        modalElement.innerHTML = modalContent;
+                        //console.log('video modal is open');
+                    } else {
+                        modalElement.innerHTML = '';
+                        //console.log('video modal is not open');
+                    }
+                });
+                
+            }
+
+
+
+            // const pixelDensity = window.devicePixelRatio;
+            // console.log("Pixel density:", pixelDensity);
+            // if( pixelDensity > 1 ) {
+            //     document.querySelector('body').classList.add('high-dpi');
+            // }
+
+
+            const loco = document.querySelectorAll('.locomotive');
+
+            if (loco.length > 0) {
+
+                const sections = document.querySelectorAll('.entry-content > .wp-block-kadence-rowlayout');
+
+                sections.forEach((section, index) => {
+                    section.setAttribute('data-scroll-section', '');
+                });
+
+                document.getElementById('colophon').setAttribute('data-scroll-section', '');
+                document.querySelector('header').setAttribute('data-scroll-section', '');
+
+                const scroll = new LocomotiveScroll({
+                  el: document.querySelector('[data-scroll-container]'),
+                  smooth: true,
+                  lerp: 0.1, // Start with a moderately responsive lerp
+                  multiplier: 0.8, // Multiplier for scroll speed
+                  scrollFromAnywhere: true
+                });
+
+            }
+
+            const no_follow = document.querySelectorAll('.no-follow');
+            console.log(no_follow);
+
+            if( no_follow.length == 0 ) {
+
+                const follow = new Follow({
+                    //debug: true,
+                    factor: 1,
+                    //attribute: 'follow',
+                    initiate: false
+                });
+    
+                setTimeout(() => {
+                    follow.initiate();
+                }, 200);
+
+            }
 
             
-
-            const follow = new Follow({
-                //debug: true,
-                factor: 1,
-                //attribute: 'follow',
-                initiate: false
-            });
-
-            setTimeout(() => {
-                follow.initiate();
-            }, 200);
             
           
         });
