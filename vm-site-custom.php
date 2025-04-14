912416bb46e3d5f89da44b639a3510de9b26efe3
Randal Pope <randalpope@Randals-MacBook-Air.local>
2025-04-14 12:23:54 -0400

    removed margins from buttons on mobile views

--- vm-site-custom.php
+++ vm-site-custom.php
@@ -3,7 +3,7 @@
  * Plugin Name: Vinyl Marketing Site Customizations
  * Plugin URI: 
  * Description: 
- * Version: 0.0.38
+ * Version: 0.0.39
  * Author: Randal Pope
  * Author URI: https://randal.io
  * License: GPL-2.0+
@@ -13,7 +13,7 @@
 
 
 function plugin_version(){
-	$version = '0.0.38';
+	$version = '0.0.39';
 	return $version;
 }
 
